575b146df5fea7da9708ce303f3b2943aa4d3498
osamuelfox <samuelsantosdesouza92@gmail.com>
2023-07-25 13:18:12 -0300

    primeiro commit

---
+++ app/src/main/java/dev/samuel/teste/agenda/view/principal/MainActivity.java
@@ -0,0 +1,19 @@
+package dev.samuel.teste.agenda.view.principal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import dev.samuel.teste.agenda.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
---
+++ app/src/main/java/dev/samuel/teste/agenda/view/principal/Nova_Conta_Activity.java
@@ -0,0 +1,16 @@
+package dev.samuel.teste.agenda.view.principal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import dev.samuel.teste.agenda.R;
+
+public class Nova_Conta_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nova_conta);
+    }
+}
